ce69d94860713ee6bfefdad49cbfdd9aca334609
alexandra <24sasha24ns@gmail.com>
2024-12-02 18:17:52 +0100

    created some text on main page + models

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 export const routes: Routes = [
 
+    { path: '', component: MainPageComponent },
     { path: 'login', component: LoginComponent },
 ];
