d6a9e2d1f00aaf58548271d10f675e29248a3596
alexandra <24sasha24ns@gmail.com>
2024-12-09 10:09:09 +0100

    created admin page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 export const routes: Routes = [
-
-    { path: '', component: MainPageComponent },
-    { path: 'login', component: LoginComponent },
+  { path: '', component: MainPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
 ];
